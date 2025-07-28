9cd05b8eff27183dc18c656b1521ee673c9be9ad
lucasxyz <lucasxyz9@gmail.com>
2025-07-27 21:16:15 -0300

    compileSdk y targetSdk cambiado a 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.burbujasgame"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.burbujasgame"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
